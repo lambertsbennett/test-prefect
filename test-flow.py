afd103eb5734e74f9ce9cd50d7deff49accd011c
Bennett Lambert <lambertsbennett@gmail.com>
2022-03-29 13:51:39 +0200

    Create test-flow.py

---
+++ test-flow.py
@@ -0,0 +1,24 @@
+from prefect import task, Flow
+from prefect.executors import DaskExecutor 
+from prefect.run_configs import KubernetesRun
+from prefect.storage import Docker
+
+
+
+@task
+def get_value():
+    return "Example!"
+
+
+@task
+def output_value(value):
+    print(value)
+
+
+with Flow("Test") as flow:
+    value = get_value()
+    output_value(value)
+
+flow.run_config = KubernetesRun(labels=None)
+flow.executor = DaskExecutor("10.2.36.44:8786")
+flow.register(project_name='test')
